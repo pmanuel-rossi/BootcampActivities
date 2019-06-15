86bd23532d38b76823a1e78b9ebcdb5fb6a37bc2
pmanuel-rossi <pedromanuel.rossi@gmail.com>
2019-06-15 12:49:44 -0500

    Create function.py

---
+++ function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 12:47:26 2019
+
+@author: pedrorossi
+"""
+
+#functions
+
+def average(list):
+    first =  sum(list)
+    print(first/len(list))
+    
+    
