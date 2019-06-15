a8872f4b4dbe151eab803669460b5178ba9b57a2
pmanuel-rossi <pedromanuel.rossi@gmail.com>
2019-06-15 11:37:28 -0500

    Finnished hobbydict Commit

--- hobbydict.py
+++ hobbydict.py
@@ -10,4 +10,8 @@ Created on Sat Jun 15 11:03:15 2019
 
 hobbyDict = {"Name" : "Pedro Rossi", "Age" : 26, "Hobbies" : ["Piano", "Tennis", "Pool"], "Wakeup": {"Monday": "6 am", "Tuesday": "5 am", "Wednsday": "6am", "Thirsday": "5 am", "Friday": "6 am"}}
 
-print(hobbyDict["Name"], )
+print(hobbyDict["Name"])
+print("Numer of hobbies: " + str(len(list(hobbyDict["Hobbies"]))))
+print("He wakes up at: ")
+for day, time in hobbyDict["Wakeup"].items():
+    print(day,time)
