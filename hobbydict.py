3055b759932836201bd2e945c2b06f85c2144537
pmanuel-rossi <pedromanuel.rossi@gmail.com>
2019-06-15 11:15:10 -0500

    Update hobbydict.py

--- hobbydict.py
+++ hobbydict.py
@@ -9,3 +9,5 @@ Created on Sat Jun 15 11:03:15 2019
 # hobbyBook
 
 hobbyDict = {"Name" : "Pedro Rossi", "Age" : 26, "Hobbies" : ["Piano", "Tennis", "Pool"], "Wakeup": {"Monday": "6 am", "Tuesday": "5 am", "Wednsday": "6am", "Thirsday": "5 am", "Friday": "6 am"}}
+
+print(hobbyDict["Name"], )
